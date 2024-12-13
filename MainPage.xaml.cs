68251dbe7bb026afce54e007154878f988bb40f9
DESKTOP-N2SOVPU\rafae <rafaelrego16@gmail.com>
2024-12-13 17:20:10 +0000

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BibliotecaRafasixteen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
